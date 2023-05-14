83aa230e38f2402e55f910d21a5ef45f407e20e7
jinwatanabe <jin.watanabe.6g@gmail.com>
2023-05-14 12:28:55 +0900

    create webcomponent sample

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,53 @@
 import { FC } from "react";
 import { createRoot, Root } from "react-dom/client";
+import { CacheProvider, css, EmotionCache } from "@emotion/react";
+import createCache from "@emotion/cache";
+import resetCss from "./reset.css?inline";
 
 const SampleElement: FC = () => {
-  return <div>Hello, world!</div>;
+  return (
+    <>
+      <h1
+        css={css`
+          color: red;
+        `}
+      >
+        Hello, world!
+      </h1>
+      <ul>
+        <li>test</li>
+      </ul>
+      <a href="#">link</a>
+    </>
+  );
 };
 
 class SampleElementComponent extends HTMLElement {
   root: Root;
+  cache: EmotionCache;
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
     this.root = createRoot(this.shadowRoot!);
+    this.cache = createCache({
+      key: "sample-element",
+      container: this.shadowRoot!,
+    });
+    this.setResetCss();
+  }
+
+  setResetCss() {
+    const style = document.createElement("style");
+    style.innerHTML = resetCss;
+    this.shadowRoot!.appendChild(style);
   }
 
   connectedCallback() {
-    this.root.render(<SampleElement />);
+    this.root.render(
+      <CacheProvider value={this.cache}>
+        <SampleElement />
+      </CacheProvider>
+    );
   }
 }
 

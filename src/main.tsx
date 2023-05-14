f2e7b63817481d2b2a26f3e302aace2bb15fae46
jinwatanabe <jin.watanabe.6g@gmail.com>
2023-05-14 11:46:11 +0900

    create react env

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+function App() {
+  return <h1>Hello Vite + React + TypeScript!</h1>;
+}
